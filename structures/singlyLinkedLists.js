db825ab284f609a4564f7907f5fbabe26687ddab
Vladimir Gulev <vladinson009@icloud.com>
2026-03-09 20:39:05 +0100

    rotate list method

--- structures/singlyLinkedLists.js
+++ structures/singlyLinkedLists.js
@@ -12,28 +12,6 @@ class SinglyLinkedList {
     this.length = 0;
     if (val) this.push(val);
   }
-  shift() {
-    if (!this.head) {
-      return null;
-    }
-    const returnValue = this.head;
-    const newHead = this.head.next;
-    this.head = newHead;
-    this.length--;
-    return returnValue;
-  }
-  unshift(value) {
-    if (!this.head) {
-      this.push(value);
-      return;
-    }
-    const newNode = new Node(value);
-    const lastHead = this.head;
-    this.head = newNode;
-    this.head.next = lastHead;
-    this.length++;
-  }
-
   push(value) {
     const newNode = new Node(value);
     if (!this.head) {
@@ -65,6 +43,32 @@ class SinglyLinkedList {
     }
     return current;
   }
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    const returnValue = this.head;
+    const newHead = returnValue.next;
+    this.head = newHead;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return returnValue;
+  }
+  unshift(value) {
+    if (!this.head) {
+      this.push(value);
+    } else {
+      const newNode = new Node(value);
+      const lastHead = this.head;
+      this.head = newNode;
+      this.head.next = lastHead;
+    }
+    this.length++;
+    return this;
+  }
+
   get(index) {
     if (index < 0 || index >= this.length) {
       return null;
@@ -80,24 +84,11 @@ class SinglyLinkedList {
     return element;
   }
   set(index, value) {
-    if (index < 0 || index >= this.length) {
+    const foundValue = this.get(index);
+    if (!foundValue) {
       return false;
     }
-
-    const newNode = new Node(value);
-
-    if (index === 0) {
-      const head = this.head;
-      this.head = newNode;
-      this.head.next = head.next;
-    } else {
-      const prev = this.get(index - 1);
-      const next = prev.next.next;
-
-      prev.next = newNode;
-      newNode.next = next;
-    }
-
+    foundValue.val = value;
     return true;
   }
   insert(index, value) {
@@ -114,8 +105,8 @@ class SinglyLinkedList {
       const futureNext = prev.next;
       prev.next = newNode;
       newNode.next = futureNext;
+      this.length++;
     }
-    this.length++;
     return true;
   }
   remove(index) {
@@ -128,22 +119,132 @@ class SinglyLinkedList {
       return this.pop();
     } else {
       const prev = this.get(index - 1);
-      const current = prev.next;
-      const next = current.next;
+      const removed = prev.next;
+      const next = removed.next;
       prev.next = next;
       this.length--;
-      return current;
+      return removed;
     }
+  }
+  rotate(num) {
+    if (this.length === 0) {
+      return this;
+    }
+    num = num % this.length;
+    if (num < 0) {
+      num = this.length + num;
+    }
+    if (num === 0) {
+      return this;
+    }
+    let newTail = this.get(num - 1);
+    let newHead = newTail.next;
+
+    this.tail.next = this.head;
+    this.head = newHead;
+    this.tail = newTail;
+    this.tail.next = null;
+    return this;
   }
 }
 
 var singlyLinkedList = new SinglyLinkedList();
-
-singlyLinkedList.push(5); // singlyLinkedList
-
-singlyLinkedList.push(10); // singlyLinkedList
-
-singlyLinkedList.push(15); // singlyLinkedList
+singlyLinkedList.push(5).push(10).push(15).push(20).push(25);
 console.log(
-  singlyLinkedList.get('gr'), // singlyLinkedList
+  singlyLinkedList.head.val, // 5
+);
+console.log(
+  singlyLinkedList.tail.val, // 25;
+);
+
+singlyLinkedList.rotate(3);
+console.log(
+  singlyLinkedList.head.val, // 20
+);
+console.log(
+  singlyLinkedList.head.next.val, // 25
+);
+
+console.log(
+  singlyLinkedList.head.next.next.val, // 5
+);
+console.log(
+  singlyLinkedList.head.next.next.next.val, // 10
+);
+console.log(
+  singlyLinkedList.head.next.next.next.next.val, // 15
+);
+console.log(
+  singlyLinkedList.tail.val, // 15
+);
+console.log(
+  singlyLinkedList.tail.next, // null
+);
+
+var singlyLinkedList = new SinglyLinkedList();
+singlyLinkedList.push(5).push(10).push(15).push(20).push(25);
+console.log(
+  singlyLinkedList.head.val, // 5
+);
+console.log(
+  singlyLinkedList.tail.val, // 25;
+);
+
+singlyLinkedList.rotate(-1);
+console.log(
+  singlyLinkedList.head.val, // 25
+);
+console.log(
+  singlyLinkedList.head.next.val, // 5
+);
+console.log(
+  singlyLinkedList.head.next.next.val, // 10
+);
+
+console.log(
+  singlyLinkedList.head.next.next.next.val, // 15
+);
+
+console.log(
+  singlyLinkedList.head.next.next.next.next.val, // 20
+);
+console.log(
+  singlyLinkedList.tail.val, // 20
+);
+
+console.log(
+  singlyLinkedList.tail.next, // null
+);
+
+var singlyLinkedList = new SinglyLinkedList();
+singlyLinkedList.push(5).push(10).push(15).push(20).push(25);
+console.log(
+  singlyLinkedList.head.val, // 5
+);
+console.log(
+  singlyLinkedList.tail.val, // 25;
+);
+
+singlyLinkedList.rotate(1000);
+console.log(
+  singlyLinkedList.head.val, // 5
+);
+
+console.log(
+  singlyLinkedList.head.next.val, // 10
+);
+console.log(
+  singlyLinkedList.head.next.next.val, // 15
+);
+console.log(
+  singlyLinkedList.head.next.next.next.val, // 20
+);
+console.log(
+  singlyLinkedList.head.next.next.next.next.val, // 25
+);
+console.log(
+  singlyLinkedList.tail.val, // 25
+);
+console.log(
+  singlyLinkedList.tail.next, // null
 );
