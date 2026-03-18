2fa8d6cf37009c8484873c9d5aeb5bda9825aa27
Vladimir Gulev <vladinson009@icloud.com>
2026-03-18 18:49:47 +0100

    Visit graph element dfs & bfs

--- structures/graph.js
+++ structures/graph.js
@@ -25,22 +25,81 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+  depthFirstSearch(start) {
+    const result = [];
+    const visited = {};
+
+    const dfsHelper = (vertex) => {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+
+      this.adjacencyList[vertex].forEach((el) => {
+        if (!visited[el]) {
+          return dfsHelper(el);
+        }
+      });
+    };
+
+    dfsHelper(start);
+    return result;
+  }
+  breadthFirstSearch(start) {
+    const result = [];
+    const queue = [];
+    const visited = { [start]: true };
+
+    const bfsHelper = (vertex) => {
+      if (!vertex) return null;
+      result.push(vertex);
+
+      this.adjacencyList[vertex].forEach((el) => {
+        if (!visited[el]) {
+          visited[el] = true;
+          queue.push(el);
+        }
+      });
+      return bfsHelper(queue.shift());
+    };
+
+    bfsHelper(start);
+    return result;
+  }
 }
 
 var graph = new Graph();
 
-graph.addVertex('A');
-graph.addVertex('B');
-graph.addVertex('C');
-graph.addVertex('D');
+graph.addVertex('S');
+graph.addVertex('P');
+graph.addVertex('U');
+graph.addVertex('X');
+graph.addVertex('Q');
+graph.addVertex('Y');
+graph.addVertex('V');
+graph.addVertex('R');
+graph.addVertex('W');
+graph.addVertex('T');
 
-graph.addEdge('A', 'B');
-graph.addEdge('A', 'C');
-graph.addEdge('B', 'D');
-graph.addEdge('C', 'D');
+graph.addEdge('S', 'P');
+graph.addEdge('S', 'U');
 
-graph.removeVertex('C');
-graph.removeVertex('B');
+graph.addEdge('P', 'X');
+graph.addEdge('U', 'X');
 
-console.log(graph.adjacencyList['A']); // still exists
-console.log(graph.adjacencyList['D']); // still exists
+graph.addEdge('P', 'Q');
+graph.addEdge('U', 'V');
+
+graph.addEdge('X', 'Q');
+graph.addEdge('X', 'Y');
+graph.addEdge('X', 'V');
+
+graph.addEdge('Q', 'R');
+graph.addEdge('Y', 'R');
+
+graph.addEdge('Y', 'W');
+graph.addEdge('V', 'W');
+
+graph.addEdge('R', 'T');
+graph.addEdge('W', 'T');
+
+console.log(graph.breadthFirstSearch('S')); // ["S", "P", "U", "X", "Q", "V", "Y", "R", "W", "T"]
